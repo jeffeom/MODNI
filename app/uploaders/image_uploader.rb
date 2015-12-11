da421fc98e607d5455854ad72ef2244b262ba348
Jeff <jeffeom@gmail.com>
2015-12-11 02:14:05 -0800

    Fixed some css and made the web faster

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,6 +44,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [200, 200]
   end
 
+  version :large do
+    process :resize_to_fit => [2000, 500]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
